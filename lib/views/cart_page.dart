0028195d793d7fe10f5db49dd79e5ec5925ec455
asmfahim1 <asmfahim1@gmail.com>
2022-03-03 16:44:22 -0800

    cart_page

--- lib/views/cart_page.dart
+++ lib/views/cart_page.dart
@@ -8,14 +8,89 @@ class CartPage extends StatelessWidget {
 
   final cartController = Get.put(CartController());
 
+  int item = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cart"),
+        backgroundColor: Color(0xFF8CA6DB),
+        title: const Center(
+          child: Text("Cart",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
-      body: Center(
-        child: Text(""),
+      body: Column(
+        children: [
+          Expanded(
+            child: GetX<CartController>(
+              builder: (controller) {
+                return ListView.builder(
+                    itemCount: controller.cartItems.length,
+                    itemBuilder: (BuildContext context,int index){
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              height: 100,
+                              width: 100,
+                              child: FlatButton(
+                                onPressed: (){},
+                                child:
+                                //_progressBarActive == false?const CircularProgressIndicator():
+                                Text(controller.cartItems[index].productName),
+
+                              ),
+                            ),
+
+                            Text("Fork Lift "),
+
+                            // SizedBox(width: 100,),
+
+                            IconButton(
+                                onPressed: (){},
+                                icon: Icon(Icons.remove)
+                            ),
+                            Text("$item"),
+                            IconButton(
+
+                                onPressed: (){},
+                                icon: Icon(Icons.add)
+                            ),
+
+                          ],
+                        ),
+                      );
+                    }
+                );
+              }
+            ),
+          ),
+
+          SizedBox(height: 10,),
+
+          FlatButton(
+            color: Color(0xFF8CA6DB),
+            onPressed: (){
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text("Order Confirmed",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ));
+            },
+            child: Text("Add"),
+          ),
+
+        ],
       ),
     );
   }
